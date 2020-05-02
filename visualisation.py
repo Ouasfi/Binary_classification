ce2bf6f0fd74ea84ccb5b4fcfd1eb550b4dfe70a
Ouasfi <amine1ouasfi@hotmail.f>
2020-05-02 16:45:24 +0200

    add confusion matrix and loss plot

--- visualisation.py
+++ visualisation.py
@@ -6,6 +6,50 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 from preprocessing.preprocessing_ckd import Preprocessing
 from pandas.plotting import scatter_matrix, parallel_coordinates
+from sklearn.metrics import confusion_matrix
+
+
+def plot_cm(labels, predictions, p=0.5):
+
+
+
+    cm = confusion_matrix(labels, predictions > p)
+    plt.figure(figsize=(5,5))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title('Confusion matrix @{:.2f}'.format(p))
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+    print('True Negatives: ', cm[0][0])
+    print('False Positives: ', cm[0][1])
+    print('False Negatives: ', cm[1][0])
+    print('True Positives: ', cm[1][1])
+    print('Total : ', np.sum(cm[1]))
+
+def performance(features, labels, model, BATCH_SIZE ):
+
+    predictions_baseline = model.predict(features, batch_size=BATCH_SIZE)
+    baseline_results = model.evaluate(features, labels,
+                                    batch_size=BATCH_SIZE, verbose=0)
+    for name, value in zip(model.metrics_names, baseline_results):
+    print(name, ': ', value)
+    print()
+
+    plot_cm(labels, predictions_baseline)
+
+def plot_loss(history, label, n):
+    
+    # Use a log scale to show the wide range of values.
+    plt.semilogy(history.epoch,  history.history['loss'],
+               color=colors[n], label='Train '+label)
+    plt.semilogy(history.epoch,  history.history['val_loss'],
+          color=colors[n], label='Val '+label,
+          linestyle="--")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+
+    plt.legend()
+
 
 def scatter_plot_matrix(file, separator=","):
     """
